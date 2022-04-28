033ea4bd4b9231f5efded56c19f1f27744894d3a
boumohamed <mb.bouzri@gmail.com>
2022-04-28 21:27:35 +0200

    to get the new version

--- src/main/java/bouzri/me/springmvc/entities/Medcin.java
+++ src/main/java/bouzri/me/springmvc/entities/Medcin.java
@@ -1,2 +1,38 @@
-package bouzri.me.springmvc.entities;public class Medcin {
+package bouzri.me.springmvc.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class Medcin {
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 4, max = 40)
+    @Column(length = 40)
+    private String nom;
+
+    @NotEmpty
+    @Size( min = 4, max = 40)
+    @Column(length = 40, unique = true)
+    private String Email;
+
+    @NotEmpty
+    @Size(min = 4, max = 40)
+    @Column(length = 40)
+    private String Speciality;
+
+
+
 }
--- src/main/java/bouzri/me/springmvc/repositories/medcinRepository.java
+++ src/main/java/bouzri/me/springmvc/repositories/medcinRepository.java
@@ -1,2 +1,7 @@
-package bouzri.me.springmvc.repositories;public interface medcinRepository {
+package bouzri.me.springmvc.repositories;
+
+import bouzri.me.springmvc.entities.Medcin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface medcinRepository extends JpaRepository<Medcin, Long> {
 }
